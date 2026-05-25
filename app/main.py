cdab506b3f22a59c84e3ec4f0c2cebb8aecd6b82
GhostKlinkz <taralaroslav@gmail.com>
2026-05-25 15:34:40 +0300

    Add Person class and create_person_list function
    Implemented Person class with initialization and spouse linking.

--- app/main.py
+++ app/main.py
@@ -1,8 +1,24 @@
 class Person:
-    # write your code here
-    pass
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    for p in people:
+        Person(p["name"], p["age"])
+
+    def link_spouse(person_dict: dict) -> Person:
+        person_instance = Person.people[person_dict["name"]]
+        spouse_key = "wife" if person_dict.get("wife") else "husband"
+        spouse_name = person_dict.get(spouse_key)
+
+        if spouse_name:
+            setattr(person_instance, spouse_key, Person.people[spouse_name])
+
+        return person_instance
+
+    return [link_spouse(p) for p in people]
