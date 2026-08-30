c30c36358acc33b8545a40e019a998b526cb9905
GhostKlinkz <taralaroslav@gmail.com>
2026-08-30 23:38:06 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -9,22 +9,21 @@ class Person:
 
 def create_person_list(people: list) -> list:
     Person.people.clear()
-    
+
     for person_dict in people:
         Person(person_dict["name"], person_dict["age"])
-        
+
     person_instances = []
     for person_dict in people:
         name = person_dict["name"]
         person_instance = Person.people[name]
-        
-        spouse_key = "wife" if "wife" in person_dict else "husband"
+
+        spouse_key = "wife" if person_dict.get("wife") else "husband"
         spouse_name = person_dict.get(spouse_key)
-        
+
         if spouse_name is not None:
             setattr(person_instance, spouse_key, Person.people[spouse_name])
-            
-        person_instances.append(person_instance)
-        
-    return person_instances
 
+        person_instances.append(person_instance)
+
+    return person_instances
